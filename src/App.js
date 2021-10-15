5d2413d5622861771d836e0faa752ad6149668db
batesmichelle23 <batesmichelle23@gmail.com>
2021-10-14 21:24:12 -0500

    Added dictionary component to app // added some css to dictionary component // created code for search bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">Hello
+    <div className="App">
+      <div className="container">
+        <header className="header">
+          <main>
+            <Dictionary />
+          </main>
+        </header>
+      </div>
     </div>
   );
 }
