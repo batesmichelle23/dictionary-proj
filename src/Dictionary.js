64a2dbe5c964a85b045387b620e3988b5386129d
batesmichelle23 <batesmichelle23@gmail.com>
2021-10-15 19:53:48 -0500

    Removed loaded images // created states for searching // created meaning and results components // created code to display results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
+import Meaning from "./Meaning";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        console.log(response.data[0].meanings[0].definitions[0].defintion);
+        setResults(response.data[0]);
     }
         
     function search(event) {
         event.preventDefault();
-        
+
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         console.log(apiUrl);
         axios.get(apiUrl).then(handleResponse);
@@ -28,6 +32,7 @@ export default function Dictionary() {
                     autoFocus={true}
                     onChange={handleKeywordChange} />
             </form>
+            <Results results={results} />
         </div>
     )
 }
