83237423eef328f88954a4111174e3367bb00455
batesmichelle23 <batesmichelle23@gmail.com>
2021-10-23 17:17:19 -0500

    Called api to show photos // added css for spacing

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,10 +2,17 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
+
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
+
+    }
 
     function handleResponse(response) {
         setResults(response.data[0]);
@@ -16,6 +23,11 @@ export default function Dictionary() {
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey = "563492ad6f9170000100000105924ec22d264708b70583d328447621";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = { Authorization : `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleKeywordChange(event) {
@@ -30,6 +42,7 @@ export default function Dictionary() {
                     onChange={handleKeywordChange} />
             </form>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
     )
 }
